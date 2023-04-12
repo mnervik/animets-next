407a2c74a28e237aae1227953b3869f397dfa040
mnervik <nervik.mathias@gmail.com>
2023-04-12 21:43:11 +0200

    Added Roboto font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import Head from 'next/head'
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
+import { Roboto } from 'next/font/google'
+
+import { Container, CssBaseline } from '@mui/material'
+
+import NavBar from '@components/navbar'
 
 import '@/styles/globals.scss'
+import 'plyr/dist/plyr.css'
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  display: 'swap',
+  subsets: ['latin']
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -11,7 +23,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>Anime</title>
       </Head>
 
-      <Component {...pageProps} />
+      <CssBaseline />
+      <NavBar />
+      <Container component='main' maxWidth={false} className={roboto.className}>
+        <Component {...pageProps} />
+      </Container>
       <Analytics />
     </>
   )
